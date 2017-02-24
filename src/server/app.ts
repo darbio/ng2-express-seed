6bcca60117a9c3781a8446f1b8d1ac7b673c9eff
darbio <james@darb.io>
2017-02-24 12:59:16 +1100

    Added OIDC server

--- src/server/app.ts
+++ src/server/app.ts
@@ -10,6 +10,7 @@ import * as passport from 'passport';
 import * as passportHttpBearer from 'passport-http-bearer';
 import * as jwtDecode from 'jwt-decode';
 import * as request from 'request';
+import * as Provider from 'oidc-provider';
 
 import index from './routes/index';
 import status from './routes/v1/status';
@@ -20,106 +21,111 @@ import { Config } from '../shared/config';
 const app: express.Express = express();
 const config: Config = new Config();
 
-// Set up bearer authentication strategy
-passport.use(new passportHttpBearer.Strategy(
-  function (token, done) {
-    let jwt = jwtDecode(token);
+const provider = new Provider("http://localhost:3000/op");
 
-    // Verify the token
-    var options = {
-        url: config.okta_server_url + '/oauth2/v1/introspect',
-        method: 'POST',
-        headers: {
-          'Content-Type' : 'application/x-www-form-urlencoded',
-          'charset' : 'UTF-8'
-        },
-        form: {
-          token: token,
-          token_type_hint: 'id_token',
-          client_id : config.client_id,
-          client_secret : config.client_secret
-        }
-    };
+provider.initialize().then(() => {
+  // Set up bearer authentication strategy
+  passport.use(new passportHttpBearer.Strategy(
+    function (token, done) {
+      let jwt = jwtDecode(token);
 
-    // Start the request
-    request(options, function (error, response, body) {
-        if (error || response.statusCode != 200) {
-          return done(error);
-        }
+      // Verify the token
+      var options = {
+          url: config.okta_server_url + '/oauth2/v1/introspect',
+          method: 'POST',
+          headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+            'charset' : 'UTF-8'
+          },
+          form: {
+            token: token,
+            token_type_hint: 'id_token',
+            client_id : config.client_id,
+            client_secret : config.client_secret
+          }
+      };
 
-        if (!error && response.statusCode == 200) {
-          var userInfo = JSON.parse(body);
-
-          if (!userInfo.active) {
-            return done("User is not active");
+      // Start the request
+      request(options, function (error, response, body) {
+          if (error || response.statusCode != 200) {
+            return done(error);
           }
 
-          let user = {
-            sub : userInfo.sub,
-            uid : userInfo.uid,
-            email : userInfo.email,
-            name : userInfo.name,
-            preferred_username : userInfo.preferred_username
-          };
+          if (!error && response.statusCode == 200) {
+            var userInfo = JSON.parse(body);
 
-          return done(error, user);
-        }
+            if (!userInfo.active) {
+              return done("User is not active");
+            }
+
+            let user = {
+              sub : userInfo.sub,
+              uid : userInfo.uid,
+              email : userInfo.email,
+              name : userInfo.name,
+              preferred_username : userInfo.preferred_username
+            };
+
+            return done(error, user);
+          }
+      });
+    }
+  ));
+
+  // Redirect all http requests to https
+  const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https' && req.hostname !== 'localhost') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+  app.use(forceSSL());
+
+  app.use(logger('combined'));
+
+  app.use('/', express.static(path.join(__dirname,'../client')));
+  app.use('/api/v1/status', passport.authenticate('bearer', { session: false }), status);
+  app.use('/api/v1/config', client_config);
+  app.use('/op', provider.callback);
+
+  // For all GET requests, send back index.html so that PathLocationStrategy can be used
+  app.all('*', (req: any, res: any) => {
+    console.log(`[TRACE] Server 404 request: ${req.originalUrl}`);
+    res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+  });
+
+  // Catch 404 and forward to error handler
+  app.use((req,res,next) => {
+    var err = new Error('Not Found');
+    err['status'] = 404;
+    next(err);
+  });
+
+  // Error handlers
+  // Development error handler - will print stacktrace
+  if(process.env.NODE_ENV === 'development') {
+    app.use((err: Error,req,res,next) => {
+      res.status(err['status'] || 500);
+      res.json({
+        title: 'error',
+        message: err.message,
+        error: err
+      });
     });
   }
-));
 
-// Redirect all http requests to https
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https' && req.hostname !== 'localhost') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-app.use(forceSSL());
-
-app.use(logger('combined'));
-
-app.use('/', express.static(path.join(__dirname,'../client')));
-app.use('/api/v1/status', passport.authenticate('bearer', { session: false }), status);
-app.use('/api/v1/config', client_config);
-
-// For all GET requests, send back index.html so that PathLocationStrategy can be used
-app.all('*', (req: any, res: any) => {
-  console.log(`[TRACE] Server 404 request: ${req.originalUrl}`);
-  res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
-});
-
-// Catch 404 and forward to error handler
-app.use((req,res,next) => {
-  var err = new Error('Not Found');
-  err['status'] = 404;
-  next(err);
-});
-
-// Error handlers
-// Development error handler - will print stacktrace
-if(process.env.NODE_ENV === 'development') {
+  // Production error handler - no stacktrace leaked to user
   app.use((err: Error,req,res,next) => {
     res.status(err['status'] || 500);
     res.json({
       title: 'error',
       message: err.message,
-      error: err
+      error: {}
     });
-  });
-}
-
-// Production error handler - no stacktrace leaked to user
-app.use((err: Error,req,res,next) => {
-  res.status(err['status'] || 500);
-  res.json({
-    title: 'error',
-    message: err.message,
-    error: {}
   });
 });
 
